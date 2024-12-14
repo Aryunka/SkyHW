5ca36466d556e229e9a69c888bc547e17bc68caa
Aryuna Zoltueva <aryuna_zoltueva@MacBook-Pro-Aryuna.local>
2024-12-15 00:08:29 +0800

    flake8 and mypy сheck

--- scr/main.py
+++ scr/main.py
@@ -1,6 +1,6 @@
-from scr.widget import get_date, mask_account_card
-
 from scr.masks import get_mask_account, get_mask_card_number
+from scr.processing import filter_by_state, sort_by_date
+from scr.widget import get_date, mask_account_card
 
 card_number = input("Номер карты ")
 account_number = input("Номер счета ")
@@ -10,3 +10,5 @@ type_card_number = input("Тип и номер ")
 string_date = input("Дата ")
 print(mask_account_card(type_card_number))
 print(get_date(string_date))
+print(filter_by_state)
+print(sort_by_date)
--- scr/processing.py
+++ scr/processing.py
@@ -1,13 +1,16 @@
 from datetime import datetime
 
 
-def filter_by_state(data, state="EXECUTED"):
+def filter_by_state(data: list[dict], state: str = "EXECUTED") -> list[dict]:
+    """Функция, которая принимает список словарей"""
 
     return [item for item in data if item.get("state") == state]
 
 
-def sort_by_date(data, order="descending"):
-    def get_datetime(item):
+def sort_by_date(data: list[dict], order: str = "descending") -> list[dict]:
+    """Функция, которая сортирует список словарей"""
+
+    def get_datetime(item: dict) -> datetime:
         return datetime.strptime(item["date"], "%Y-%m-%dT%H:%M:%S.%f")
 
     if order.lower() == "ascending":
